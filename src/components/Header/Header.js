b3e5fed4ad6dfceaef623992eb995253420c9abf
Daniil Khlyvniuk <daniil.khlyv@gmail.com>
2021-08-20 01:16:43 +0300

    started header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Lifes from "../Lifes/Lifes"
+
+const Header = () => {
+    return (
+        <div>
+            <Lifes/>
+
+        </div>
+    );
+};
+
+export default Header;
